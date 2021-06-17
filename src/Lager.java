12c6ad755f140f7d356f5592751b21892cf34d97
Irrenhauspatient <andreasscheuer85@gmx.de>
2021-06-17 16:52:01 +0200

    schrott

--- src/Lager.java
+++ src/Lager.java
@@ -1,5 +1,6 @@
 import java.util.*;
 import java.util.function.BiConsumer;
+import java.util.function.BiPredicate;
 import java.util.function.Consumer;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
